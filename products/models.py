00e589d9b1361e31fd4b6bab108c93cd47f2cb61
kapcak <simeonbd@gmail.com>
2021-07-05 10:14:58 +0300

    Fixed description in models.Product

--- products/models.py
+++ products/models.py
@@ -10,6 +10,7 @@ class ProductCategory(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='products_images', blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
