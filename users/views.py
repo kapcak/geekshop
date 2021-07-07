b8241010f492e0aba1e61fbf73e4080d807ff4d4
kapcak <simeonbd@gmail.com>
2021-07-07 11:27:14 +0300

    Added logout

--- users/views.py
+++ users/views.py
@@ -38,3 +38,8 @@ def registration(request):
         'form': form,
     }
     return render(request, 'users/registration.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
