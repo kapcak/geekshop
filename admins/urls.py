9aafdb115e01cd17e9ecec1d24b81c100edca5a9
kapcak <simeonbd@gmail.com>
2021-07-16 16:59:18 +0300

    Added "remove" and "update" controllers for admins

--- admins/urls.py
+++ admins/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from admins.views import admin_users, admin_users_create, admin_users_update_delete, index
+from admins.views import admin_users, admin_users_create, admin_users_update, admin_users_remove, index
 
 app_name = 'admins'
 urlpatterns = [
     path('', index, name='index'),
     path('users/', admin_users, name='admin_users'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users/create/', admin_users_create, name='admin_users_create'),
+    path('users/update/<int:pk>/', admin_users_update, name='admin_users_update'),
+    path('users/remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
 ]
