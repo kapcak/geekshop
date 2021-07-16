260f8777d98a0f70c4d25ab4493e759bee7ceb0a
kapcak <simeonbd@gmail.com>
2021-07-16 16:26:19 +0300

    Added "Create" for admins users

--- admins/views.py
+++ admins/views.py
@@ -1,20 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'admins/index.html')
+    context = {'title': 'Админ-Панель'}
+    return render(request, 'admins/index.html', context)
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {'title': 'Админ-Панель - Пользователи', 'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Админ-Панель - Создание Пользователя',
+        'form': form,
+    }
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 def admin_users_update_delete(request):
