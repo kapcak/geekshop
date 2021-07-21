4fcd4ffbc9721563572a902505e6d5018ee5a375
kapcak <simeonbd@gmail.com>
2021-07-21 14:04:24 +0300

    Added CBV for UpdateView

--- admins/urls.py
+++ admins/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from admins.views import UserCreateView, UserListView, admin_users_update, admin_users_remove, index, admin_product_read, admin_product_update, admin_product_remove, admin_product_create
+from admins.views import UserCreateView, UserListView, UserUpdateView, admin_users_remove, index, admin_product_read, admin_product_update, admin_product_remove, admin_product_create
 
 app_name = 'admins'
 urlpatterns = [
     path('', index, name='index'),
     path('users/', UserListView.as_view(), name='admin_users'),
     path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
-    path('users/update/<int:pk>/', admin_users_update, name='admin_users_update'),
+    path('users/update/<int:pk>/', UserUpdateView.as_view(), name='admin_users_update'),
     path('users/remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
     path('products/', admin_product_read, name='admin_product_read'),
     path('products/update/<int:pk>/', admin_product_update, name='admin_product_update'),
--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse, reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from users.models import User
 from products.models import Product
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductPropertyForm, ProductAddForm
@@ -51,23 +51,30 @@ class UserCreateView(CreateView):
 #     return render(request, 'admins/admin-users-create.html', context)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, pk):
-    selected_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(
-            instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=selected_user)
-    context = {
-        'title': 'Админ-панель - Редактирование пользователя',
-        'form': form,
-        'selected_user': selected_user,
-    }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_update(request, pk):
+#     selected_user = User.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = UserAdminProfileForm(
+#             instance=selected_user, files=request.FILES, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     else:
+#         form = UserAdminProfileForm(instance=selected_user)
+#     context = {
+#         'title': 'Админ-панель - Редактирование пользователя',
+#         'form': form,
+#         'selected_user': selected_user,
+#     }
+#     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 @user_passes_test(lambda u: u.is_staff)
