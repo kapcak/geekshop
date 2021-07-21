f27a5c381e8cae66ca07a31c082847aa73548169
kapcak <simeonbd@gmail.com>
2021-07-21 13:51:29 +0300

    Added CBV for ListView and CreateView

--- admins/urls.py
+++ admins/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from admins.views import admin_users, admin_users_create, admin_users_update, admin_users_remove, index, admin_product_read, admin_product_update, admin_product_remove, admin_product_create
+from admins.views import UserCreateView, UserListView, admin_users_update, admin_users_remove, index, admin_product_read, admin_product_update, admin_product_remove, admin_product_create
 
 app_name = 'admins'
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
-    path('users/create/', admin_users_create, name='admin_users_create'),
+    path('users/', UserListView.as_view(), name='admin_users'),
+    path('users/create/', UserCreateView.as_view(), name='admin_users_create'),
     path('users/update/<int:pk>/', admin_users_update, name='admin_users_update'),
     path('users/remove/<int:pk>/', admin_users_remove, name='admin_users_remove'),
     path('products/', admin_product_read, name='admin_product_read'),
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from users.models import User
 from products.models import Product
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductPropertyForm, ProductAddForm
@@ -14,28 +16,39 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {'title': 'Админ-Панель - Пользователи',
-               'users': User.objects.all()}
-    return render(request, 'admins/admin-users-read.html', context)
+
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users(request):
+#     context = {'title': 'Админ-Панель - Пользователи',
+#                'users': User.objects.all()}
+#     return render(request, 'admins/admin-users-read.html', context)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(
-            data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {
-        'title': 'Админ-Панель - Создание Пользователя',
-        'form': form,
-    }
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    template_name = 'admins/admin-users-create.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_create(request):
+#     if request.method == 'POST':
+#         form = UserAdminRegistrationForm(
+#             data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:users'))
+#     else:
+#         form = UserAdminRegistrationForm()
+#     context = {
+#         'title': 'Админ-Панель - Создание Пользователя',
+#         'form': form,
+#     }
+#     return render(request, 'admins/admin-users-create.html', context)
 
 
 @user_passes_test(lambda u: u.is_staff)
