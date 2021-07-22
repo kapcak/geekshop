2f2562945bfc3292ff8a9a3af53629ab9af8c9a6
kapcak <simeonbd@gmail.com>
2021-07-22 16:04:12 +0300

    Added method_decorator for admins users

--- admins/views.py
+++ admins/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse, reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.decorators import method_decorator
 from users.models import User
 from products.models import Product
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductPropertyForm, ProductAddForm
@@ -16,21 +17,18 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
-
 class UserListView(ListView):
     model = User
     template_name = 'admins/admin-users-read.html'
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['title'] = 'Админ-Панель - Пользователи'
         return context
 
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users(request):
-#     context = {'title': 'Админ-Панель - Пользователи',
-#                'users': User.objects.all()}
-#     return render(request, 'admins/admin-users-read.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserCreateView(CreateView):
@@ -39,21 +37,9 @@ class UserCreateView(CreateView):
     template_name = 'admins/admin-users-create.html'
     success_url = reverse_lazy('admins:admin_users')
 
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_create(request):
-#     if request.method == 'POST':
-#         form = UserAdminRegistrationForm(
-#             data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:users'))
-#     else:
-#         form = UserAdminRegistrationForm()
-#     context = {
-#         'title': 'Админ-Панель - Создание Пользователя',
-#         'form': form,
-#     }
-#     return render(request, 'admins/admin-users-create.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserUpdateView(UpdateView):
@@ -62,24 +48,9 @@ class UserUpdateView(UpdateView):
     template_name = 'admins/admin-users-update-delete.html'
     success_url = reverse_lazy('admins:admin_users')
 
-
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_update(request, pk):
-#     selected_user = User.objects.get(id=pk)
-#     if request.method == 'POST':
-#         form = UserAdminProfileForm(
-#             instance=selected_user, files=request.FILES, data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:admin_users'))
-#     else:
-#         form = UserAdminProfileForm(instance=selected_user)
-#     context = {
-#         'title': 'Админ-панель - Редактирование пользователя',
-#         'form': form,
-#         'selected_user': selected_user,
-#     }
-#     return render(request, 'admins/admin-users-update-delete.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserDeleteView(DeleteView):
@@ -93,15 +64,9 @@ class UserDeleteView(DeleteView):
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
-
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_remove(request, pk):
-#     user = User.objects.get(id=pk)
-#     user.is_active = False
-#     user.save()
-#     return HttpResponseRedirect(reverse('admins:admin_users'))
-
-
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 @user_passes_test(lambda u: u.is_staff)
