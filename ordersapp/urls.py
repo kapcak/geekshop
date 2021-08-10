b41d9beaa268ff27efddc180fdd25b445c364198
kapcak <simeonbd@gmail.com>
2021-08-10 14:48:10 +0300

    Added orders update

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -6,7 +6,7 @@ app_name = 'ordersapp'
 urlpatterns = [
     path('', views.OrderList.as_view(), name='orders_list'),
     path('read/<pk>/', views.OrderList.as_view(), name='order_read'),
-    path('update/<pk>/', views.OrderList.as_view(), name='order_update'),
+    path('update/<pk>/', views.OrderItemUpdate.as_view(), name='order_update'),
     path('delete/<pk>/', views.OrderList.as_view(), name='order_delete'),
     path('create/', views.OrderItemCreate.as_view(), name='order_create'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,12 +1,14 @@
+from django.http import request
 from ordersapp.forms import OrderItemForm
 from django.db import models
 from django.forms.models import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.db import transaction
 
 from ordersapp.models import Order, OrderItem
+from baskets.models import Basket
 
 
 # Create your views here.
@@ -29,7 +31,15 @@ class OrderItemCreate(CreateView):
         if self.request.method == 'POST':
             formset = OrderFormset(self.request.POST)
         else:
-            formset = OrderFormset()
+            basket_items = Basket.objects.filter(user=self.request.user)
+            if basket_items.exists():
+                OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=basket_items.count())
+                formset = OrderFormset()
+                for num, form in enumerate(formset.forms):
+                    form.initial['product'] = basket_items[num].product
+                    form.initial['quantity'] = basket_items[num].quantity
+            else:
+                formset = OrderFormset()
 
 
         data['orderitems'] = formset
@@ -45,5 +55,38 @@ class OrderItemCreate(CreateView):
             if orderitems.is_valid():
                 orderitems.instance = self.object
                 orderitems.save()
+                basket_items = Basket.objects.filter(user=self.request.user)
+                basket_items.delete()
+
+        return super().form_valid(form)
+
+class OrderItemUpdate(UpdateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy("ordersapp:orders_list")
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.method == 'POST':
+            formset = OrderFormset(self.request.POST, instance=self.object)
+        else:
+            formset = OrderFormset(instance=self.object)
+
+
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
 
         return super().form_valid(form)
